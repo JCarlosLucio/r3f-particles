b91cf55a154c68319504d187136159a79faf9e63
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-11 02:42:41 -0600

    Add PlaneNoise to PlanetNoiseScene - Center PlaneNoise - Add OrbitControls

--- src/components/PlaneNoiseScene.tsx
+++ src/components/PlaneNoiseScene.tsx
@@ -1,14 +1,19 @@
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Center, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import PlaneNoise from './PlaneNoise';
 
 const PlaneNoiseScene = () => {
   return (
     <Canvas dpr={window.devicePixelRatio}>
       <color attach="background" args={['#111972']} />
-      <pointLight position={[-20, 10, 25]} intensity={0.5} />
-
-      <PerspectiveCamera makeDefault fov={75} position={[8, 5, 40]} />
-      <OrbitControls autoRotate autoRotateSpeed={3} />
+      <Suspense fallback={null}>
+        <Center>
+          <PlaneNoise />
+        </Center>
+      </Suspense>
+      <PerspectiveCamera makeDefault fov={75} position={[0, 0, 10]} />
+      <OrbitControls />
     </Canvas>
   );
 };
