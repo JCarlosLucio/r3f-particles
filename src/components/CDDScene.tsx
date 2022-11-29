367d64242e15b3b18eded699e368867470c17fee
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-29 01:41:10 -0600

    Add CDDScene component

--- src/components/CDDScene.tsx
+++ src/components/CDDScene.tsx
@@ -1,15 +1,16 @@
 import { OrbitControls, OrthographicCamera } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, Props } from '@react-three/fiber';
 import { ReactNode } from 'react';
 import Lights from './Lights';
 
 interface CDDSceneProps {
   children: ReactNode;
+  props?: Props;
 }
 
-const CDDScene = ({ children }: CDDSceneProps) => {
+const CDDScene = ({ children, ...props }: CDDSceneProps) => {
   return (
-    <Canvas>
+    <Canvas shadows dpr={window.devicePixelRatio} {...props}>
       <color attach="background" args={['#111972']} />
       {children}
       <OrthographicCamera makeDefault position={[-30, 30, 30]} zoom={10} />
